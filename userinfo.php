af942ce075088bd699032bac5898becba0e75938
usha122020 <75332220+usha122020@users.noreply.github.com>
2021-11-09 16:32:08 +0530

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -16,18 +16,6 @@ else
 	echo "no connection" ;
 }
 
-mysqli_select_db($con,'uuser');
-
-$username=$_POST['username'];
-
-$password=$_POST['password'];
-
-$query="insert into userinfo (username,password) values ('$username','$password')";
-
-mysqli_query($con,$query);
-
-echo "$query" ;
-
 
 
 ?>
